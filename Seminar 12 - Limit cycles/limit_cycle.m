0a9e4e8995c3ed06ece35898b974cfe1e3852777
AlexShem <alex.shemendyuk@gmail.com>
2023-11-21 18:09:13 +0100

    Update limit_cycle.m

--- Seminar 12 - Limit cycles/limit_cycle.m
+++ Seminar 12 - Limit cycles/limit_cycle.m
@@ -1,47 +1,60 @@
-%% Фазовый портрет
-eps = -.5;
-R = 1;
+%% Phase Portrait Section
+% This section generates the phase portrait of the dynamical system.
 
+% Parameters
+epsilon = -0.5;
+radius = 1;
+
+% Create a grid in the x-y plane
 [x, y] = meshgrid(linspace(-2, 2, 101));
-r = sqrt(x.^2 + y.^2);
-phi = linspace(0, 2*pi);
+radial_distance = sqrt(x.^2 + y.^2);
+angle_phi = linspace(0, 2*pi);
 
-dx = y + eps*x.*(R - r);
-dy = -x + eps*y.*(R - r);
+% Define the dynamics of the system
+dx = y + epsilon * x .* (radius - radial_distance);
+dy = -x + epsilon * y .* (radius - radial_distance);
 
+% Plotting the phase portrait
 figure(1);
-streamslice(x, y, dx, dy)
+streamslice(x, y, dx, dy);
 xlabel('x');
 ylabel('y');
 hold on;
-plot(R*cos(phi), R*sin(phi), '-r', 'LineWidth', 1.5);
+plot(radius * cos(angle_phi), radius * sin(angle_phi), '-r', 'LineWidth', 1.5);
 hold off;
 axis equal;
 
-%% Сечение Пуанкаре
-z_0 = [0, 1.02;
-    0, .9;
-    0, .5];
-P_const = nan(size(z_0, 1), 1);
-tspan = [0, 2*pi];
+%% Poincaré Section
+% This section analyzes the system's behavior over time using Poincaré sections.
 
+% Initial conditions
+initial_conditions = [0, 1.02; 0, 0.9; 0, 0.5];
+poincare_constants = nan(size(initial_conditions, 1), 1);
+time_span = [0, 2*pi];
+
+% Plotting for different initial conditions
 figure(2)
-for k = 1 : size(z_0, 1)
-    [t, z] = ode45(@(t, z) cycle_syst(t, z, eps, R), tspan, z_0(k, :));
-    P_const(k) = z(end, 2) / z_0(k, 2);
+for k = 1 : size(initial_conditions, 1)
+    [t, z] = ode45(@(t, z) cycle_system(t, z, epsilon, radius), time_span, initial_conditions(k, :));
+    poincare_constants(k) = z(end, 2) / initial_conditions(k, 2);
     plot(z(:, 1), z(:, 2)); hold on;
 end
-plot(z_0(:, 1), z_0(:, 2), '*r');
-plot(R*cos(phi), R*sin(phi), '-r', 'LineWidth', 2);
+plot(initial_conditions(:, 1), initial_conditions(:, 2), '*r');
+plot(radius * cos(angle_phi), radius * sin(angle_phi), '-r', 'LineWidth', 2);
 hold off;
 axis equal;
 axis([-2 2 -2 2]);
 
-%% Система
-function dz = cycle_syst(~, z, eps, R)
-dz = zeros(2, 1);
-r = sqrt(z(1)^2 + z(2)^2);
+%% System Dynamics Definition
+% This function defines the dynamics of the system.
 
-dz(1) = z(2) + eps * z(1) * (R - r);
-dz(2) = -z(1) + eps * z(2) * (R - r);
+function dz = cycle_system(~, z, epsilon, radius)
+% Calculating the rate of change of the system's state.
+
+dz = zeros(2, 1);
+radial_distance = sqrt(z(1)^2 + z(2)^2);
+
+% Dynamics equations
+dz(1) = z(2) + epsilon * z(1) * (radius - radial_distance);
+dz(2) = -z(1) + epsilon * z(2) * (radius - radial_distance);
 end
